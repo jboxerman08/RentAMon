a9c20caee0423aa3857c37357b86c6e7839b0b72
Jeremy Antoine <jeremincanada@gmail.com>
2021-09-22 14:11:56 -0400

    change length of description back to 150

--- app/models/monument.rb
+++ app/models/monument.rb
@@ -6,7 +6,7 @@ class Monument < ApplicationRecord
     # validates :coordinates, presence: true not needed as we have longitude and latitude now
     validates :longitude, presence: true
     validates :latitude, presence: true
-    validates :description, length: { maximum: 300,
+    validates :description, length: { maximum: 150,
     too_long: "%{count} characters is the maximum allowed" }
     validates :price, numericality: { only_integer: true }
 end
