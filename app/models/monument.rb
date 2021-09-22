8e77081ac9e7e6f403562958657979228a256e8c
Francesco Perrozzi <francescoperrozzi3@gmail.com>
2021-09-21 21:27:14 -0400

    added monumnet validations

--- app/models/monument.rb
+++ app/models/monument.rb
@@ -1,2 +1,10 @@
 class Monument < ApplicationRecord
+    validates :name, presence: true
+    validates :availability, presence: true
+    validates :price, presence: true
+    validates :description, presence: true
+    validates :coordinates, presence: true
+    validates :description, length: { maximum: 150,
+    too_long: "%{count} characters is the maximum allowed" }
+    validates :price, numericality: { only_integer: true }
 end
