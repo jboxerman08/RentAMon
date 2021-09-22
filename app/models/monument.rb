13b3bc4575158c7d4760e71f3be4e948d1f73cdc
Jeremy Antoine <jeremincanada@gmail.com>
2021-09-21 20:20:19 -0400

    added the photo: to mnument modele and index view DONT FORGET TO PERMIT in controller and to include it in the form

--- app/models/monument.rb
+++ app/models/monument.rb
@@ -1,5 +1,5 @@
 class Monument < ApplicationRecord
-
+has_many_attached :photos
   validates :name, presence: true
   validates :description, presence: true
   validates :coordinates, presence: true
