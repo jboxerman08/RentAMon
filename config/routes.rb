437b30b63f5b80f20fc837b2326999a1d4dd7c43
Jeremy Antoine <jeremincanada@gmail.com>
2021-09-23 11:37:21 -0400

    corrections on the router - rental should be nested in the monument

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :monuments, only: [:index, :show, :create, :new]
-  resources :rentals, only: [:create, :show, :new, :index, :edit, :update]
+  resources :monuments, only: [:index, :show, :create, :new] do
+    resources :rentals, only: [:create, :show, :new, :index, :edit, :update]
+  end
 end
 
 # JOURNEY
