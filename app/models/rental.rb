455a6fb184edb5b8eb1b12e0bdc023b602c411ac
Jeremy Antoine <jeremincanada@gmail.com>
2021-09-23 11:41:04 -0400

    corrections on the rental model - rental should be nested in the monument

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,4 +1,10 @@
 class Rental < ApplicationRecord
   belongs_to :user
   belongs_to :monument
+    # monument cannot be blank
+  validates :monument_id, allow_blank: false, presence: true
+  # user cannot be blank
+  validates :user_id, allow_blank: false, presence: true
+  # is unique for a given user/monument couple
+  validates :monument_id, uniqueness: { scope: :user_id }
 end
