42f341ba9689806268bffdbfce3d3f1616508368
Jeremy Antoine <jeremincanada@gmail.com>
2021-09-23 12:58:33 -0400

    change the user model to add monuments through rental

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :rentals
+  has_many :monuments, through: :rental
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
