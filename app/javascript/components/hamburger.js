14c7a444975377ac5cea9b49f62ea1a9eb74af32
Jeremy Antoine <jeremincanada@gmail.com>
2021-09-24 12:38:12 -0400

    js works not pretty but it works

---
+++ app/javascript/components/hamburger.js
@@ -0,0 +1,16 @@
+
+// const hamburger = () => {
+//   console.log('Hello')
+// const menu = document.getElementById("menu")
+// const nav = document.getElementById("nav")
+// const menuBg = document.getElementById("menu-bg")
+
+// // I want to toggle class menu to class change when I click on it
+// // I want to toggle class nav to class change when I click on it
+// // I want to toggle class menuBg to change-bg when I click on it
+// menu.addEventListener ("click", (event) => {
+//   console.log(event);
+//     console.log(event.currentTarget);
+// };
+
+// export { hamburger };
