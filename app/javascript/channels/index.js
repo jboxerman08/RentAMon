0c561d0b7b3d2517679b19a3e82f077eb1f8b66f
Jeremy Antoine <jeremincanada@gmail.com>
2021-09-24 11:16:07 -0400

    hamburger created in CSS and html and creation of Components folder in JS; creation of shared folder in views (for footer and navbar)

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -3,3 +3,15 @@
 
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
+
+// start hamburger js
+const menu = document.getElementById("menu")
+const nav = document.getElementById("nav")
+const menuBg = document.getElementById("menu-bg")
+
+function onClickMenu(){
+  menu.classList.toggle("change");
+  nav.classList.toggle("change");
+  menuBg.classList.toggle("change-bg");
+};
+// end hamburger js
