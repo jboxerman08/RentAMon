0cd1a742dc6c95fb052e486617e89d7cc193fc24
Jeremy Boxerman <jboxerman08@gmail.com>
2021-09-24 22:52:18 -0400

    added about page route and linked it in footer

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'monuments#index'
+
+  get '/about', to: 'pages#about'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :monuments, only: [:index, :show, :create, :new]
   resources :rentals, only: [:create, :show, :new, :index, :edit, :update]
