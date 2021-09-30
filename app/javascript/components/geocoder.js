03d3fc3a6ba3c912c8036c012d135d1fb4a64c63
Jeremy Boxerman <jboxerman08@gmail.com>
2021-09-30 19:11:49 -0400

    changed max-zoom in for single markers

--- app/javascript/components/geocoder.js
+++ app/javascript/components/geocoder.js
@@ -8,7 +8,7 @@ const initMapbox = () => {
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+  map.fitBounds(bounds, { padding: 70, maxZoom: 6, duration: 1000 });
 };
 
   if (mapElement) {
